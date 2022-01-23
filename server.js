b8673807755329cafca763628d178949ae9cfc1f
Ranaco <ranasatyamraj@gmail.com>
2022-01-23 15:56:04 +0530

    error done

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
  
-fs.readFile("./files/starter.txt", (err, data) => {
+fs.readFile("./files/index.txt", 'utf8', (err, data) => {
     if(err) throw err;
-    console.log(data.toString());
+    console.log(data);
+})
+
+
+process.on("uncaughtException", err => {
+    console.error(`There was an uncaught exception: ${err}`);
+    process.exit(1);
 })
