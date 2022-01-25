899cebc8bb18416a8439faecbe0c4aed77122c2c
Ranaco <ranasatyamraj@gmail.com>
2022-01-25 19:20:18 +0530

    Strating with express

--- Day_3/server.js
+++ Day_3/server.js
@@ -4,11 +4,15 @@ const fsPromise = require('fs').promises;
 const app = express();
 
 //Finding the port here
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 5000;
 
-app.get('/', async (req, res) => {
+app.get('^/$|/index.html|index', async (req, res) => {
     const data = await fsPromise.readFile('./views/index.html', 'utf8');
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+
+app.get('/new_page.html', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new_page.html'));
 })
 
 //Listening to the server when it starts
